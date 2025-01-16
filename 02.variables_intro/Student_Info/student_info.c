062fab6bc23dfe729839f59f1687392c9b1d620b
jweeks7910 <jweeks7910@floridapoly.edu>
2025-01-16 13:32:02 +0000

    Code first program

---
+++ 02.variables_intro/Student_Info/student_info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int course_id = 2271;
+    int student_id = 7910;
+    printf("Name: Jonah Weeks\n");
+    printf("Course ID: %d\n", course_id);
+    printf("Student ID: %d\n", student_id);
+    printf("Student ID - Course ID is: %d\n", student_id - course_id);
+
+    return 0;
+}
